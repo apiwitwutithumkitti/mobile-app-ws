f7ab8172cc04940f92793c01a123ac3eaef0987c
apiwitwutithumkitti <apiwit.wutithumkitti@outlook.com>
2021-12-04 21:06:20 +0700

    Add log.info, debug for userController Get All Users, Specific User Create User Update User Delete User

--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/shared/dto/AddressDTO.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/shared/dto/AddressDTO.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.app.ws.mobileappws.shared.dto;
 
+import lombok.Data;
+
+@Data
 public class AddressDTO {
 	private long id;
 	private String addressId;
@@ -9,55 +12,5 @@ public class AddressDTO {
 	private String postalCode;
 	private String types;
 	private UserDto userDetails;
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getAddressId() {
-		return addressId;
-	}
-	public void setAddressId(String addressId) {
-		this.addressId = addressId;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getStreetName() {
-		return streetName;
-	}
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
-	}
-	public String getPostalCode() {
-		return postalCode;
-	}
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-	public String getTypes() {
-		return types;
-	}
-	public void setTypes(String types) {
-		this.types = types;
-	}
-	public UserDto getUserDetails() {
-		return userDetails;
-	}
-	public void setUserDetails(UserDto userDetails) {
-		this.userDetails = userDetails;
-	}
-	
 	
 }
--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/model/response/AddressesRest.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/model/response/AddressesRest.java
@@ -2,6 +2,11 @@ package com.appsdeveloperblog.app.ws.mobileappws.ui.model.response;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AddressesRest extends RepresentationModel<AddressesRest> {
 	private String addressId;
 	private String city;
@@ -9,43 +14,6 @@ public class AddressesRest extends RepresentationModel<AddressesRest> {
 	private String streetName;
 	private String postalCode;
 	private String types;
-	
-	public String getAddressId() {
-		return addressId;
-	}
-	public void setAddressId(String addressId) {
-		this.addressId = addressId;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getStreetName() {
-		return streetName;
-	}
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
-	}
-	public String getPostalCode() {
-		return postalCode;
-	}
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-	public String getTypes() {
-		return types;
-	}
-	public void setTypes(String types) {
-		this.types = types;
-	}
 	
 
 }
